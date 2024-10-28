848aea2fbf13684a5544e2b424c55f8bfd075c72
miguel <mi_lmc@hotmail.com>
2024-10-28 20:10:56 -0300

    test: add sumAll test

--- 4.arrays-and-slices/sum_test.go
+++ 4.arrays-and-slices/sum_test.go
@@ -15,3 +15,12 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1,2},[]int{0,9,10})
+	want:= []int{3,19}
+
+	if got != want{
+		t.Errorf("got %v want %v",got, want)
+	}
+}
