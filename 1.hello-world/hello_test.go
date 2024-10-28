afca4121bd181d8165bc13d55a17dfe45292b19a
Miguel <mi_lmc@hotmail.com>
2024-10-28 15:13:56 -0300

    refactor: optimize hello function and test

--- 1.hello-world/hello_test.go
+++ 1.hello-world/hello_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying Hello to people", func (t *testing.T)  {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+	
+		assertCorrectMessage(t,got,want)
+	})
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want:= "Hello, World"
+
+		assertCorrectMessage(t,got,want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string){
+	t.Helper()
+
+	if got != want{
+		t.Errorf("got %q want %q",got,want)
 	}
 }
