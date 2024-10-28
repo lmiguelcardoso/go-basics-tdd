f66adc9ce7bb3166feecc213f05bd2d5b2f26546
miguel <mi_lmc@hotmail.com>
2024-10-28 20:04:05 -0300

    test: adding test to slice input in sum method

--- 4.arrays-and-slices/sum_test.go
+++ 4.arrays-and-slices/sum_test.go
@@ -3,12 +3,26 @@ package arraysandslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers:= [5]int{1,2,3,4,5}
+	t.Run("array of 5 number",func(t *testing.T) {
 
-	got:= Sum(numbers)
-	want:= 15
+		numbers:= [5]int{1,2,3,4,5}
+		
+		got:= Sum(numbers)
+		want:= 15
+		
+		if got != want{
+			t.Errorf("got %v want %v",got, want)
+		}
+	})
 
-	if got != want{
-		t.Errorf("got %v want %v",got, want)
-	}
+	t.Run("slice of any size", func(t *testing.T) {
+		numbers:= []int{3,4,5,6}
+		
+		got:=Sum(numbers)
+		want:= 18
+
+		if got != want{
+			t.Errorf("got %v want %v",got, want)
+		}
+	})
 }
