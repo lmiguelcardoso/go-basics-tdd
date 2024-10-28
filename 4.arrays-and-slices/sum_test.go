43897a2d57645fc82dd9df059514ff929e002baf
miguel <mi_lmc@hotmail.com>
2024-10-28 20:09:05 -0300

    feat: implement sum method to run with slices

--- 4.arrays-and-slices/sum_test.go
+++ 4.arrays-and-slices/sum_test.go
@@ -3,17 +3,6 @@ package arraysandslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("array of 5 number",func(t *testing.T) {
-
-		numbers:= [5]int{1,2,3,4,5}
-		
-		got:= Sum(numbers)
-		want:= 15
-		
-		if got != want{
-			t.Errorf("got %v want %v",got, want)
-		}
-	})
 
 	t.Run("slice of any size", func(t *testing.T) {
 		numbers:= []int{3,4,5,6}
