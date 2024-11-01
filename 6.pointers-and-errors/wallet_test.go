98cdd805a2ff28ce82ce86a6328703d6b50d2b9a
Miguel <mi_lmc@hotmail.com>
2024-11-01 08:55:08 -0300

    refactor: assert function to check wallet test cases

--- 6.pointers-and-errors/wallet_test.go
+++ 6.pointers-and-errors/wallet_test.go
@@ -3,30 +3,30 @@ package pointersanderrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
+	assertBalance := func (t testing.TB, wallet Wallet, want Bitcoin)  {
+		t.Helper()
+		got := wallet.balance
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+
+		assertBalance(t,wallet,want)		
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t,wallet,want)		
 	})
 }
