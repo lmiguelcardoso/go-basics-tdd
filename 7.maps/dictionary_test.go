069c4619ee18a1947a42b23b8e3d7a287c49e2e9
Miguel <mi_lmc@hotmail.com>
2024-11-05 09:56:01 -0300

    feat: delete method on dictionary struct

--- 7.maps/dictionary_test.go
+++ 7.maps/dictionary_test.go
@@ -72,6 +72,17 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+
+	assertError(t, err, ErrNotFound)
+}
+
 // Asserts
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
