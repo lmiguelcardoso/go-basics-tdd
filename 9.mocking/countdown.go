16b7b6018416a89a7b333960019ca49030980bd1
Miguel <mi_lmc@hotmail.com>
2024-11-08 15:38:20 -0300

    refactor: implement configurable sleeper

--- 9.mocking/countdown.go
+++ 9.mocking/countdown.go
@@ -20,6 +20,14 @@ type Sleeper interface {
 	Sleep()
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
